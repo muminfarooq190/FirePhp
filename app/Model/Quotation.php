cead1a301a73bd94e1d0210d60595c3902b4f182
muminfarooq190 <muminfarooq586@gmail.com>
2021-12-25 09:32:36 +0530

    linked models and controllers

--- app/Model/Quotation.php
+++ app/Model/Quotation.php
@@ -6,5 +6,19 @@ namespace app\Model;
 
 class Quotation extends Model
 {
+    public function GetQueries()
+    {
+        $Query=$this->createQuery();
+        $this->query($Query);
 
+    }
+
+    private function  createQuery(){
+        $Query = "Insert into Quotations values ('$this->hotelname',
+                               '$this->hotelAddress','$this->hotelRoomType','$this->hotelRating',
+                               '$this->Inclusions','$this->Exclusions','$this->Itenary',
+                               '$this->Flight','$this->Flight')";
+
+        return $Query;
+    }
 }
